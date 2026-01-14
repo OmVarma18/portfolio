4ed66be023d425c08ba466db4b91717a8993caef
OmVarma9 <omvarma369@gmail.com>
2026-01-14 18:46:03 +0530

    Enable basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // basePath: isProd ? "/portfolio" : "",
+  basePath: isProd ? "/portfolio" : "",
   images: {
     unoptimized: true,
   }
